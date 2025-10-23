8dc21226d67d1f5a2e79a6d9489bd3b9b72388f9
Alexandr Malafeev <alxm@Alxm-MBP.local>
2025-10-23 20:28:01 +0300

    switch back to yt-dlp

--- src/youtube-dl.ts
+++ src/youtube-dl.ts
@@ -95,11 +95,7 @@ export const downloadFromInfo = (
 	const process = spawn("yt-dlp", [
 		"--newline",  // Выводить прогресс построчно для легкого парсинга
 		"--no-playlist",
-		//"--merge-output-format", "mp4",
-		"--postprocessor-args",
-		"ffmpeg: -c:v libx265 -crf 23 preset medium -c:a aac -b:a 128k -tag:v hvc1 -movflags +faststart",
-		"-o",
-		"--downloader ffmpeg",
+		"-t mp4",
 		output,
 		...args,
 		info.webpage_url || info.url || "",
