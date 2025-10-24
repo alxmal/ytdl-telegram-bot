4e3e2b87fa31b5c8b1858a6af6884c783934db54
Alexandr Malafeev <alxm@Alxm-MBP.local>
2025-10-24 15:53:31 +0300

    fix codecs issues

--- src/youtube-dl.ts
+++ src/youtube-dl.ts
@@ -95,6 +95,7 @@ export const downloadFromInfo = (
 	const process = spawn("yt-dlp", [
 		"--newline",  // Выводить прогресс построчно для легкого парсинга
 		"--no-playlist",
+		"-o",
 		output,
 		...args,
 		info.webpage_url || info.url || "",
